1fcab38d2c0712b1ff746d54814f5a66e311194f
Akanksha0802-ss <your_github_email@example.com>
2025-08-27 12:48:16 +0530

    Updated Module-6 in Articles Component of Lecture-6

--- Module-6/routing-basic-template/src/components/Articles/Articles.jsx
+++ Module-6/routing-basic-template/src/components/Articles/Articles.jsx
@@ -1,12 +1,28 @@
 import React from 'react'
+import { useSearchParams } from "react-router-dom"
+
 
 const Articles = () => {
+  const [searchParams, setSearchParams] = useSearchParams();
+  const sortBy = searchParams.get("sortBy");
+  const category = searchParams.get("category");
+  const sortByViews = searchParams.get("sortByViews");
+
+  const handleSortBy = () => {
+    setSearchParams({
+      sortByViews: "Views",
+      category: "Electronics",
+    })
+  }
+
   return (
     <div>
         <h2>
             Articles
-            <p>SortBy: Category:</p>
-        </h2>    
+            <p>SortBy:{sortBy ?? sortByViews} Category: {category}
+            </p>
+        </h2>  
+        <button onClick={handleSortBy}>Sort By Views</button>  
     </div>
   )
 }
