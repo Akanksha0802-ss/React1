93f6660eaf653155cfe990c50a7e3472e22c74ee
Akanksha0802-ss <your_github_email@example.com>
2025-08-27 14:39:18 +0530

    Updated Module-6 in Articles Components of Lecture-7

--- Module-6/routing-basic-template/src/components/Articles/Articles.jsx
+++ Module-6/routing-basic-template/src/components/Articles/Articles.jsx
@@ -1,28 +1,55 @@
 import React from 'react'
 import { useSearchParams } from "react-router-dom"
 
+const dummyArticles = [
+  { id: 1, title: "Laptop Review", views: 250, category: "electronics"},
+  { id: 2, title: "Smartphone Tips", views: 250, category: "electronics"},
+  { id: 3, title: "Running Shoes", views: 100, category: "fashion"},
+  { id: 4, title: "Washing Machine Guide", views: 150, category: "electronics"},
+]
 
 const Articles = () => {
   const [searchParams, setSearchParams] = useSearchParams();
+  console.log(searchParams, setSearchParams);
   const sortBy = searchParams.get("sortBy");
   const category = searchParams.get("category");
   const sortByViews = searchParams.get("sortByViews");
+  let filteredArticle = dummyArticles;
+
 
   const handleSortBy = () => {
     setSearchParams({
-      sortByViews: "Views",
-      category: "Electronics",
+      sortByViews: "true",
+      category: "electronics",
     })
+  }
+
+  if(category) {
+    filteredArticle = filteredArticle.filter(
+      (article) => article.category === category
+    )
+    // console.log(filteredArticle);
+  }
+  if (sortByViews === "true") {
+    filteredArticle = [...filteredArticle].sort((a, b)  => b.views - a.views)
   }
 
   return (
     <div>
         <h2>
             Articles
-            <p>SortBy:{sortBy ?? sortByViews} Category: {category}
+            <p>
+              SortBy:{sortBy ?? (sortByViews === "true" ? "views" : "none")} |Category: {category ?? "All"}
             </p>
         </h2>  
         <button onClick={handleSortBy}>Sort By Views</button>  
+        <ul>
+          {filteredArticle.map((article) => (
+            <li key={article.id}>
+              {article.title} - {article.views} views ({article.category})
+            </li>
+          ))}
+        </ul>
     </div>
   )
 }
