11853581b84a48dd63a1fea34a30dd856f86dfa9
Akanksha0802-ss <your_github_email@example.com>
2025-09-01 15:36:24 +0530

    Updatof Lecture-6

--- Module-7/routing-basic-template/src/components/Admin/Sellers.jsx
+++ Module-7/routing-basic-template/src/components/Admin/Sellers.jsx
@@ -1,8 +1,10 @@
 import axios from "axios";
 import React, { useEffect, useState} from "react";
+import Loader from "../common/Loader";
 
 const Sellers = () => {
        const [users, setUsers] = useState([]);
+       const [isLoading, setIsLoading] = useState(false);
      // const [name, setName] = useState("");
     // useEffect(()=>{
        // document.title = `Name is ${name}`;
@@ -13,20 +15,25 @@ const Sellers = () => {
        // console.log("Component UnMount.!!");
        // };
     // }, [name]);
-    useEffect(() => {
-       axios.get("https://jsonplaceholder.typicode.com/users").then((res) => { 
-          setUsers(res.data);
-    })
-      // axios
+
+    // axios
       //   .get("https://jsonplaceholder.typicode.com/users")
       //   .then((res) => console.log(res.data));
       // fetch("https://jsonplaceholder.typicode.com/users")
       //   .then((res) => res.json())
       //   .then((data) => console.log(data)).catch((error)=>{ });
+    useEffect(() => {
+      setIsLoading(true);
+       axios.get("https://jsonplaceholder.typicode.com/users").then((res) => { 
+          setUsers(res.data);
+          setIsLoading(false);
+    })
   }, []);
+  // if(isLoading) return <h3>Loading..</h3>
     return (
       <>
         <h3>Admin Sellers Page</h3>
+        {isLoading && <Loader />}
         {users.map((user) => (
           <p key={user.id}>{user.name}</p>
         ))}
---
+++ Module-7/routing-basic-template/src/components/common/Loader.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Loader.css';
+
+const Loader = () => {
+  return (
+    <div>
+       <div class="lds-ripple">
+        <div></div>
+        <div></div>
+        </div>
+    </div>
+  )
+}
+
+export default Loader;
