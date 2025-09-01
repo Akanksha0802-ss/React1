66126d0fbe97072cfa7f769117e40265a74ca153
Akanksha0802-ss <your_github_email@example.com>
2025-09-01 15:00:43 +0530

    Updated Module-7 of Lecture-5

--- Module-7/routing-basic-template/src/components/Admin/Sellers.jsx
+++ Module-7/routing-basic-template/src/components/Admin/Sellers.jsx
@@ -1,25 +1,37 @@
+import axios from "axios";
 import React, { useEffect, useState} from "react";
 
 const Sellers = () => {
-  const [name, setName] = useState("");
-    useEffect(()=>{
+       const [users, setUsers] = useState([]);
+     // const [name, setName] = useState("");
+    // useEffect(()=>{
        // document.title = `Name is ${name}`;
       // const heading = document.querySelector("h3");
        // heading.style.color = "red"; 
-       console.log("Component Mount.!!");
-       return () => {
-         console.log("Component UnMount.!!");
-       };
-    }, [name]);
+       // console.log("Component Mount.!!");
+       // return () => {
+       // console.log("Component UnMount.!!");
+       // };
+    // }, [name]);
+    useEffect(() => {
+       axios.get("https://jsonplaceholder.typicode.com/users").then((res) => { 
+          setUsers(res.data);
+    })
+      // axios
+      //   .get("https://jsonplaceholder.typicode.com/users")
+      //   .then((res) => console.log(res.data));
+      // fetch("https://jsonplaceholder.typicode.com/users")
+      //   .then((res) => res.json())
+      //   .then((data) => console.log(data)).catch((error)=>{ });
+  }, []);
     return (
       <>
         <h3>Admin Sellers Page</h3>
-        <input type='text' onChange={(e) => {
-          setName(e.target.value);
-        }}
-        />
+        {users.map((user) => (
+          <p key={user.id}>{user.name}</p>
+        ))}
       </>  
-    ) 
+    ); 
 };
 
 export default Sellers;
